53feee4acb081f7aa8cad55159200f877668ff8f
Serg Tsarikovskiy <s.tsarikovskiy@nullgr.com>
2017-12-15 15:13:09 +0200

    add files

---
+++ Example/Pods/Target Support Files/Pods-ThinCreditCard_Example/Pods-ThinCreditCard_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ThinCreditCard_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ThinCreditCard_Example
+@end
---
+++ Example/Pods/Target Support Files/ThinCreditCard/ThinCreditCard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ThinCreditCard : NSObject
+@end
+@implementation PodsDummy_ThinCreditCard
+@end
